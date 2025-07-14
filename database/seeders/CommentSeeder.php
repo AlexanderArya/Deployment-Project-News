da97704944395f9428c7ecfcbab0534f13dc943b
Andrean Bimo <bimowibowo4846@gmail.com>
2025-07-14 23:36:42 +0700

    Update seeder and migration changes

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class CommentSeeder extends Seeder
 {
@@ -12,6 +13,25 @@ class CommentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('comments')->insert([
+            [
+                'user_id' => Str::uuid(),
+                'comment' => 'This anime is amazing!',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'user_id' => Str::uuid(),
+                'comment' => 'Looking forward to the next episode!',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'user_id' => Str::uuid(),
+                'comment' => 'I prefer the manga version.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
